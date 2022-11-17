ca02076615e1a51cf992d25711fa303731665c55
Kanezal <58330966+Kanezal@users.noreply.github.com>
2022-11-17 20:10:16 +0000

    add post api

--- api/inspector.js
+++ api/inspector.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const axios = require('axios');
-const { response } = require('express');
+const express = require('express')
+const axios = require('axios')
+const { response } = require('express')
 const router = express.Router()
+require('dotenv/config')
 
 /**
  * GET reports
@@ -14,7 +15,7 @@ router.get('/', (req, res) => {
             method: 'get',
             url: 'https://discord.com/api/channels/1026204385417633902/messages?limit=100',
             headers: {
-                'Authorization': 'Bot MTAzMTk0MjU0MDcwMzgyNTk0MQ.GDuJnG.aa6dwq0YCwHsyYD83I0JFudLvsJ6Y7amK1Rmpw',
+                'Authorization': process.env.BOT_TOKEN,
             }
         };
 
@@ -31,5 +32,41 @@ router.get('/', (req, res) => {
         return res.status(500).send("Server error")
     }
 })
+
+router.post('/', (req, res) => {
+    try {
+        var data = JSON.stringify({
+            "embeds": [
+                {
+                    "description": req.body.description,
+                    "author": req.body.author,
+                }
+            ]
+        });
+
+        var config = {
+            method: 'post',
+            url: 'https://discord.com/api/channels/1026204385417633902/messages',
+            headers: {
+                'Authorization': process.env.BOT_TOKEN,
+                'Content-Type': 'application/json',
+            },
+            data: data
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+            
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send("Server error")
+    }
+})
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 const PORT  = process.env.PORT || 5050
 
+app.use(express.json())
 app.use("/api/inspector", inspector)
 
 app.listen(PORT, () => {
