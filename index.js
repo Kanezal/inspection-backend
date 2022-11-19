89d0d83750ff290e716a6ee7fcfd2bed341fa50f
undefined <undefined>
2022-11-19 22:01:04 +0300

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
-
-const inspector = require("./api/inspector")
+const cors = require('cors')
 
 const app = express()
 
+const inspector = require("./api/inspector")
+
 const PORT  = process.env.PORT || 5050
 
+app.use(cors())
 app.use(express.json())
 app.use("/api/inspector", inspector)
 
